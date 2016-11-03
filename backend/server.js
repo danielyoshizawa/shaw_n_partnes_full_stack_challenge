cae5da56480d3dc2db74767b1ec05d1c24a29c92
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-11-03 16:12:40 -0200

    Adding a function to check if the word is a palindrome or not

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,34 @@
 
-var express = Require('express');
+var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var checkIfPalindrome = function(palindrome) {
+
+    var onlyLetterAndNumbers = palindrome.replace(/[^A-Z0-9]/ig, "");
+    var reversedWord = onlyLetterAndNumbers.split("").reverse().join("");
+
+    return reversedWord.localeCompare(onlyLetterAndNumbers);
+};
+
 
 app.post('/check_palindrome', urlencodedParser, function (req, res) {
     response = {
         palindrome:req.body.palindrome
     };
-    console.log(response);
-    res.end(JSON.stringify(response));
+
+    if (checkIfPalindrome(response.palindrome) == 0) {
+        res.end("200");
+    } else {
+        res.end("400");
+    }
 });
 
 var server = app.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("App listening at http://%s:%s", host, port);
 });
